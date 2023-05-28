9386b2be7ef19495c4d24fa2436f1cdfbef02732
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2023-05-28 04:25:15 +0300

    Update ShipmentList.jsx

--- src/components/ShipmentList.jsx
+++ src/components/ShipmentList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Icon } from '@material-ui/core';
-import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+// import { Icon } from '@material-ui/core';
+// import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import {
   Container,
   CardWrapper,
@@ -110,7 +110,7 @@ export const ShipmentList = ({ shipments }) => {
                 </Pag>
               </Wrap>
               <WrapArrow>
-                <Icon component={ArrowForwardIcon} />
+                {/* <Icon component={ArrowForwardIcon} /> */}
               </WrapArrow>
               <Wrap>
                 <Pag> Кінцевий пункт:</Pag>
