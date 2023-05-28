8d0e8630964ffe275b6bf5d5ee5c6f92e5b239a5
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2023-05-28 17:13:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/logictranss">
+    <BrowserRouter basename="/LogicTranss">
       <App />
     </BrowserRouter>
   </StrictMode>
