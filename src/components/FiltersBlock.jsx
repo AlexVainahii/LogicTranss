7f725efd8c1d890f068b1826d4aabda4d6aebaa4
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2023-05-28 21:42:42 +0300

    qw

--- src/components/FiltersBlock.jsx
+++ src/components/FiltersBlock.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { TextField, Button } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles(theme => ({
   root: {
