43916bdf145cba6d671f529be3cc9702fb6b616c
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2023-05-28 22:58:32 +0300

    Maps

--- src/components/MapWithRoute.jsx
+++ src/components/MapWithRoute.jsx
@@ -1,71 +1,35 @@
 import React, { useEffect, useRef } from 'react';
-
-//import mapboxgl from 'mapbox-gl';
-import mapboxgl from 'mapbox-gl/dist/mapbox-gl-csp';
-
-mapboxgl.accessToken =
-  'pk.eyJ1Ijoia2FzdGllbCIsImEiOiJjbGk1cm41c2YxaXdmM2xscHV3Y3pyZTFpIn0.t1krxDv5Pa232Pr6C0QSTw';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 const MapWithRoute = ({ coordinates, centrMap, markers, zoom = 6 }) => {
   const mapContainerRef = useRef(null);
   const mapRef = useRef(null);
 
   useEffect(() => {
-    // Ініціалізація мапи
-    console.log(coordinates, '1coor');
-    console.log(centrMap, '1c');
-    console.log(markers, '1m');
-    console.log(zoom, '1z');
+    mapRef.current = L.map(mapContainerRef.current).setView(centrMap, zoom);
 
-    mapRef.current = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: centrMap,
-      zoom: 5,
-    });
-    console.table(coordinates, centrMap, markers, zoom, '2');
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+      maxZoom: 18,
+    }).addTo(mapRef.current);
+
     // Додавання маркерів
-    // markers.forEach(coord => {
-    //   //var coord1[0]=coord[1];
-    //   new mapboxgl.Marker().setLngLat(coord).addTo(mapRef.current);
-    // });
-    console.table(coordinates, centrMap, markers, zoom, '3');
-    // Додавання лінії маршруту
-    const routeLine = {
-      type: 'Feature',
-      properties: {},
-      geometry: {
-        type: 'LineString',
-        coordinates: coordinates,
-      },
-    };
-    console.table(coordinates, centrMap, markers, zoom, '4');
-    mapRef.current.on('load', () => {
-      mapRef.current.addSource('route', {
-        type: 'geojson',
-        data: {
-          type: 'FeatureCollection',
-          features: [routeLine],
-        },
-      });
-
-      mapRef.current.addLayer({
-        id: 'route',
-        type: 'line',
-        source: 'route',
-        layout: {
-          'line-join': 'round',
-          'line-cap': 'round',
-        },
-        paint: {
-          'line-color': '#277',
-          'line-width': 6,
-        },
-      });
+    markers.forEach(coord => {
+      L.marker(coord).addTo(mapRef.current);
     });
+
+    // Додавання лінії маршруту
+    const routeLine = L.polyline(coordinates).addTo(mapRef.current);
+
+    // Збільшення масштабу, щоб лінія маршруту була видимою
+    mapRef.current.fitBounds(routeLine.getBounds());
 
     // Очищення мапи під час розмонтування компонента
-    return () => mapRef.current.remove();
+    return () => {
+      mapRef.current.remove();
+    };
   }, [coordinates, centrMap, zoom, markers]);
 
   return <div ref={mapContainerRef} style={{ height: '400px' }} />;
