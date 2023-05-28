4a3d3bb321cf7b2931f305a10ae23d812ceb20c4
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2023-05-28 21:56:22 +0300

    Update FiltersBlock.jsx

--- src/components/FiltersBlock.jsx
+++ src/components/FiltersBlock.jsx
@@ -1,62 +1,64 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import { makeStyles } from '@mui/styles';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    marginBottom: theme.spacing(2),
-  },
-  textField: {
-    marginRight: theme.spacing(2),
-    width: '200px',
-  },
-  button: {
-    marginLeft: theme.spacing(2),
-  },
-}));
 
 export const FiltersBlock = ({
   filters,
   handleFilterChange,
   handleResetFilters,
 }) => {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
+    <div
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        marginBottom: 2,
+      }}
+    >
       <TextField
-        className={classes.textField}
+        sx={{
+          marginRight: 2,
+          width: '200px',
+        }}
         label="Номер перевезення"
         name="shipmentNumber"
         value={filters.shipmentNumber}
         onChange={handleFilterChange}
       />
       <TextField
-        className={classes.textField}
+        sx={{
+          marginRight: 2,
+          width: '200px',
+        }}
         label="Статус"
         name="status"
         value={filters.status}
         onChange={handleFilterChange}
       />
       <TextField
-        className={classes.textField}
+        sx={{
+          marginRight: 2,
+          width: '200px',
+        }}
         label="Початковий пункт"
         name="originCity"
         value={filters.originCity}
         onChange={handleFilterChange}
       />
       <TextField
-        className={classes.textField}
+        sx={{
+          marginRight: 2,
+          width: '200px',
+        }}
         label="Кінцевий пункт"
         name="destinationCity"
         value={filters.destinationCity}
         onChange={handleFilterChange}
       />
       <Button
-        className={classes.button}
+        sx={{
+          marginLeft: 2,
+        }}
         variant="contained"
         color="primary"
         onClick={handleResetFilters}
