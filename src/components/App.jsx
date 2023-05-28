0132811405d3b586720f612f842aa2aa5074f124
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2023-05-28 16:45:38 +0300

    start refresh

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,16 @@
-import { Route, Routes } from 'react-router-dom';
-import { About } from '../pages/About';
-import { Home } from '../pages/Home';
-import { ShipmentDetails } from '../pages/ShipmentDetails';
-import { Shipments } from '../pages/Shipment';
-import { Mission } from './Mission';
-import { Team } from './Team';
-import { Reviews } from './Reviews';
-import { SharedLayout } from './SharedLayout';
-import { Order } from 'pages/Order';
-
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<Home />} />
-        <Route path="/about" element={<About />}>
-          <Route path="mission" element={<Mission />} />
-          <Route path="team" element={<Team />} />
-          <Route path="reviews" element={<Reviews />} />
-        </Route>
-        <Route path="/shipments" element={<Shipments />} />
-        <Route path="/shipments/:id" element={<ShipmentDetails />} />
-        <Route path="/order" element={<Order />}></Route>
-      </Route>
-    </Routes>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import { StrictMode, React } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter basename="/logictranss">
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
